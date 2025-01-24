ec0917dd1b814d74ba84865c9003734ef3b79204
Wellington <well_rafa@live.com>
2025-01-23 22:50:20 -0300

    Moc da rota login

--- Program.cs
+++ Program.cs
@@ -3,4 +3,23 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapPost("/login", (LoginDTO loginDTO)=> {
+    if (loginDTO.Email == "admin@teste.com" && loginDTO.Senha == "123456")
+    {
+        return Results.Ok("Login com sucesso");
+    }
+    else
+    {
+        return Results.Unauthorized();
+    }
+
+});
+
 app.Run();
+
+public class LoginDTO
+{
+    public string Email { get; set; } = default!;
+
+    public string Senha { get; set; } = default!;
+}
